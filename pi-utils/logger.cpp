3ae21779caacb44d5a9bc629f5d2837f8246ddd3
Denis Kudia <dkudja@gmail.com>
2017-10-03 18:16:14 -0700

    Added external logger spdlog

--- pi-utils/logger.cpp
+++ pi-utils/logger.cpp
@@ -9,31 +9,35 @@
 #include <time.h>
 #include <chrono>
 #include <ctime>
+
 #include "logger.h"
 
 namespace logger {
+
+Logger plog;
 
 std::chrono::time_point<std::chrono::system_clock> tp;
 char mtime[30];
 
 void log(const LLOG level, const std::string pattern, const std::string message){
-	//struct timespec tm;
-	//clock_gettime(CLOCK_REALTIME, &tm);
-	//std::cout << (tm.tv_sec%126000) << ":" << (tm.tv_sec%3600) << ":" << (tm.tv_sec%60) << " | " << level << " | " << pattern << " | "<< message << std::endl;
+/*
 	tp = std::chrono::system_clock::now();
 	std::time_t time_now = std::chrono::system_clock::to_time_t(tp);
 	std::strftime(mtime, sizeof(mtime), "%T", std::localtime(&time_now));
 	std::cout << mtime << " | " << level << " | " << pattern << " | "<< message << std::endl;
+*/
+	plog.log(level, pattern, message);
 }
-
 
 Logger::Logger() {
-	// TODO Auto-generated constructor stub
-
-}
-
-Logger::~Logger() {
-	// TODO Auto-generated destructor stub
+	size_t q_size = 4096; //queue size must be power of 2
+	//spdlog::set_async_mode(q_size);
+	//spdlog::set_pattern("%H:%M:%S %z|%t|%v");
+	async_file = spdlog::daily_logger_st("async_file_logger", "/home/denis/logs/async_log");
+	async_file->set_level(spdlog::level::debug);
+	//async_file->flush_on(spdlog::level::err);s
+	async_file->set_pattern("%H:%M:%S %z|%t|%v");
+	
 }
 
 } /* namespace logger */
--- pi-utils/logger.h
+++ pi-utils/logger.h
@@ -9,6 +9,7 @@
 #define PI_LIBRARY_LOGGER_H_
 
 #include <string>
+#include "spdlog/spdlog.h"
 
 namespace logger {
 
@@ -24,7 +25,24 @@ void log(const LLOG level, const std::string pattern, const std::string msg);
 class Logger {
 public:
 	Logger();
-	virtual ~Logger();
+	
+	virtual ~Logger() {
+		async_file->flush();
+	}
+
+	void log(const LLOG level, const std::string pattern, const std::string message){
+		if(level == LLOG::INFO)
+			async_file->info("{0}|{1}", pattern, message);
+		else if(level == LLOG::DEBUG)
+			async_file->debug("{0}|{1}", pattern, message);
+		else if(level == LLOG::NECECCARY)
+			async_file->warn("{0}|{1}", pattern, message);
+		else if(level == LLOG::ERROR)
+			async_file->error("{0}|{1}", pattern, message);
+	}
+
+private:
+	std::shared_ptr<spdlog::logger> async_file;	
 };
 
 } /* namespace logger */
