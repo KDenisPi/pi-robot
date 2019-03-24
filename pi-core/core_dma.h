32507034716c0091ce10a8c9f6c395e3297c31e8
Denis Kudia <dkudja@gmail.com>
2019-03-23 18:42:32 -0700

    DMA First test

--- pi-core/core_dma.h
+++ pi-core/core_dma.h
@@ -52,8 +52,8 @@ struct dma_regs_t{
 #define DMA_REG_CS_DISDEBUG (1<<29)         //Disable Debug
 #define DMA_REG_CS_WAIT_FOR_OUTSTANDING_WRITES (1<<28)
 //bits 27:24 not used
-#define DMA_REG_CS_PANIC_PRIORITY(val)  ((val & 0xf) << 20)
-#define DMA_REG_CS_PRIORITY(val)  ((val & 0xf) << 16)
+#define DMA_REG_CS_PANIC_PRIORITY(val)  ((val & 0xf) << 20) //AXI panic priority level. Zero is lowest priority
+#define DMA_REG_CS_PRIORITY(val)        ((val & 0xf) << 16) //AXI priority level. Zero is lovest priority
 //bits 15:9 not used
 #define DMA_REG_CS_ERROR    (1<<8)          //DMA Error, Read Only
 //bit 7 not used
@@ -76,10 +76,10 @@ struct dma_regs_t{
 #define DMA_REG_TI_SRC_DREQ         (1<<10)                   //Control Source Reads with DREQ (1 - The DREQ selected by PERMAP will gate the source read)
 #define DMA_REQ_TI_SRC_WIDTH_128    (1<<9)                    //Source transfer width (1-128bit, 0-32bit)
 #define DMA_REQ_TI_SRC_INC          (1<<8)                    //Source address increment (1-Yes, 0-No) Increment value is on depend of SRC_WIDTH
-#define DMA_REG_TI_DEST_IGNORE      (1<<11)                   //Ignore reads
-#define DMA_REG_TI_DEST_DREQ        (1<<10)                   //Control Destination Reads with DREQ (1 - The DREQ selected by PERMAP will gate the destination read)
-#define DMA_REQ_TI_DEST_WIDTH_128   (1<<9)                    //Destination transfer width (1-128bit, 0-32bit)
-#define DMA_REQ_TI_DEST_INC         (1<<8)                    //Destination address increment (1-Yes, 0-No) Increment value is on depend of DEST_WIDTH
+#define DMA_REG_TI_DEST_IGNORE      (1<<7)                    //Ignore reads
+#define DMA_REG_TI_DEST_DREQ        (1<<6)                    //Control Destination Reads with DREQ (1 - The DREQ selected by PERMAP will gate the destination read)
+#define DMA_REQ_TI_DEST_WIDTH_128   (1<<5)                    //Destination transfer width (1-128bit, 0-32bit)
+#define DMA_REQ_TI_DEST_INC         (1<<4)                    //Destination address increment (1-Yes, 0-No) Increment value is on depend of DEST_WIDTH
 #define DMA_REG_TI_WAIT_RESP        (1<<3)                    //Wait for a Write Respose (1-Wait, 0-Don't wait)
 #define DMA_REG_TI_TDMODE_2D        (1<<1)                    //1 - 2D mode, 0 - linear mode
 #define DMA_REG_TI_INTEN            (1<<0)                    //Interrupt Enable (1-Generate interrupt when transfer complited, 0-Do not generate Iterrupt)
@@ -96,12 +96,27 @@ struct dma_regs_t{
 */
 class DmaControlBlock {
 public:
-    DmaControlBlock(const DREQ dreq) : _dreq(dreq), _ti_flags(0) {
-        _ctrk_blk = static_cast<struct dma_ctrl_blk_t*>(malloc(sizeof(struct dma_ctrl_blk_t)));
+    DmaControlBlock(const DREQ dreq = DREQ::NO_required) : _dreq(dreq), _ti_flags(0) {
+        logger::log(logger::LLOG::DEBUG, "DmaCtrl", std::string(__func__));
+
+        _ctrk_blk = static_cast<struct dma_ctrl_blk_t*>( malloc(sizeof(struct dma_ctrl_blk_t)) );
+        if(_ctrk_blk == nullptr){
+            logger::log(logger::LLOG::ERROR, "DmaCtrl", std::string(__func__) + " Could not allocate memory for DMA CB");
+            return;
+        }
         memset((void*)_ctrk_blk, 0x00, sizeof(struct dma_ctrl_blk_t));
+
+        if( _dreq == DREQ::PWM){
+            set_ti_flags(ti_flags_pwm);
+        }
+        else if(_dreq == DREQ::NO_required){
+            set_ti_flags(_ti_flags_test);
+        }
     }
 
     ~DmaControlBlock(){
+        logger::log(logger::LLOG::DEBUG, "DmaCtrl", std::string(__func__));
+
         if( _ctrk_blk){
             free((void*)_ctrk_blk);
         }
@@ -109,10 +124,19 @@ public:
 
     /*
     * Set default flags for TI register
+    *
+    * DK: Not clear why we use DMA_REG_TI_NO_WIDE_BURSTS here (TODO: check without)
+    *
+    * PWM flags: DMA_REG_TI_NO_WIDE_BURSTS | DMA_REG_TI_WAIT_RESP | DMA_REG_TI_DEST_DREQ | DMA_REG_TI_PERMAP(5) | DMA_REQ_TI_SRC_INC
+    * Memory test flags: DMA_REG_TI_NO_WIDE_BURSTS | DMA_REG_TI_WAIT_RESP | DMA_REQ_TI_SRC_INC | DMA_REQ_TI_DEST_INC
+    *
     */
     void set_ti_flags(const uint32_t ti_flags){
         _ti_flags = ti_flags;
     }
+
+    const uint32_t ti_flags_pwm = DMA_REG_TI_NO_WIDE_BURSTS | DMA_REG_TI_WAIT_RESP |  DMA_REQ_TI_SRC_INC | DMA_REG_TI_DEST_DREQ | DMA_REG_TI_PERMAP(5);
+    const uint32_t _ti_flags_test = DMA_REG_TI_NO_WIDE_BURSTS | DMA_REG_TI_WAIT_RESP | DMA_REQ_TI_SRC_INC | DMA_REQ_TI_DEST_INC;
 
     /*
     * Initialize DMA control block
@@ -120,6 +144,7 @@ public:
     * txfr_len - length in bytes (!)
     */
     bool Initialize(const uintptr_t src_addr, const uintptr_t dest_addr, const uint16_t txfr_len){
+
         _ctrk_blk->_ti = _ti_flags;
         _ctrk_blk->_src_addr = src_addr;
         _ctrk_blk->_dst_addr = dest_addr;
@@ -139,7 +164,7 @@ private:
 */
 class DmaControl {
 public:
-    DmaControl(const unsigned short dma) : _addr(0), _dma(dma){
+    DmaControl(const unsigned short dma = 10) : _addr(0), _dma(dma){
         assert(dma < 15);
         _addr = dma_address(dma);
 
@@ -169,6 +194,7 @@ public:
     */
     bool Initialize() {
         logger::log(logger::LLOG::DEBUG, "DmaCtrl", std::string(__func__));
+
         _dma_regs = piutils::map_memory<struct dma_regs_t>(_addr);
         if(_dma_regs == nullptr){
             logger::log(logger::LLOG::ERROR, "DmaCtrl", std::string(__func__) + " Fail to initialize DMA control");
@@ -189,13 +215,10 @@ public:
     }
 
     /*
-    * Start to process DMA Control block
+    * Reset DMA
     */
-    bool process_control_block(const uintptr_t dma_control_block) {
-        //Check did we finis process the current DMA CB
-        if( !is_finished() ){
-            return false;
-        }
+    void reset(){
+        logger::log(logger::LLOG::DEBUG, "DmaCtrl", std::string(__func__));
 
         _dma_regs->_cs = DMA_REG_CS_RESET;  //Reset DMA
         std::this_thread::sleep_for(std::chrono::microseconds(10));
@@ -204,10 +227,27 @@ public:
         _dma_regs->_cs = DMA_REG_CS_INT | DMA_REG_CS_END;
         std::this_thread::sleep_for(std::chrono::microseconds(10));
 
-        //set DMA Control Block
-        _dma_regs->_conblk_ad = dma_control_block;
         //clear error flags
         _dma_regs->_debug = DMA_REG_DEBUG_READ_ERROR | DMA_REG_DEBUG_FIFO_ERROR | DMA_REG_DEBUG_READ_LAST_NOT_SET_ERROR;
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+
+    /*
+    * Start to process DMA Control block
+    */
+    bool process_control_block(const uintptr_t dma_control_block) {
+        logger::log(logger::LLOG::DEBUG, "DmaCtrl", std::string(__func__));
+
+        //Check did we finis process the current DMA CB
+        if( !is_finished() ){
+            logger::log(logger::LLOG::DEBUG, "DmaCtrl", std::string(__func__) + " DMA is still busy");
+            return false;
+        }
+
+        reset();
+
+        //set DMA Control Block
+        _dma_regs->_conblk_ad = dma_control_block;
         _dma_regs->_cs = _cs_flags | DMA_REG_CS_ACTIVE;
 
         return true;
@@ -222,11 +262,17 @@ public:
 
     /*
     * Set default flags for CS register
+    *
+    * flags PWM: DMA_REG_CS_WAIT_FOR_OUTSTANDING_WRITES | DMA_REG_CS_PANIC_PRIORITY(15) | DMA_REG_CS_PRIORITY(15)
+    * flag memory test: DMA_REG_CS_WAIT_FOR_OUTSTANDING_WRITES
+    *
     */
     void set_cs_flags(const uint32_t cs_flags){
         _cs_flags = cs_flags;
     }
 
+    static const uint32_t cs_flags_pwm = DMA_REG_CS_WAIT_FOR_OUTSTANDING_WRITES | DMA_REG_CS_PANIC_PRIORITY(15) | DMA_REG_CS_PRIORITY(15);
+    static const uint32_t cs_flags_test = DMA_REG_CS_WAIT_FOR_OUTSTANDING_WRITES;
 
 private:
     uint32_t _addr; //DMA register address
--- pi-tests/test-dma/test_dma.cpp
+++ pi-tests/test-dma/test_dma.cpp
@@ -3,8 +3,42 @@
 
 #include "core_dma.h"
 
+/*
+* Simple DMA test.
+* Copy memory buffer to another one using DMA 10
+*
+*/
 int main (int argc, char* argv[])
 {
+    size_t buff_size = 1024;
+    size_t buff_size_bytes = sizeof(uint32_t) * buff_size;
+    uint32_t* src = new uint32_t(buff_size);
+    uint32_t* dst = new uint32_t(buff_size);
 
+    std::cout << "Starting..." << std::endl;
+
+
+    memset((void*)src, 0, buff_size_bytes);
+    memset((void*)src, 0, buff_size_bytes);
+
+    pi_core::core_dma::DmaControlBlock* cb = new pi_core::core_dma::DmaControlBlock();
+    cb->Initialize(reinterpret_cast<std::uintptr_t>(src), reinterpret_cast<std::uintptr_t>(dst), buff_size_bytes);
+
+    std::cout << "DMA Control Block Initialized" << std::endl;
+
+    pi_core::core_dma::DmaControl* dma = new pi_core::core_dma::DmaControl();
+    dma->Initialize();
+    std::cout << "DMA Initialized" << std::endl;
+
+    dma->process_control_block(reinterpret_cast<std::uintptr_t>(cb));
+    std::cout << "Start to process DMA Control Block" << std::endl;
+
+    for(;;){
+        if(dma->is_finished()){
+            break;
+        }
+    }
+
+    std::cout << "Finished" << std::endl;
     exit(EXIT_SUCCESS);
 }
