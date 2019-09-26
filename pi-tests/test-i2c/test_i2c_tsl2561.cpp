e77c94705f37bbd16b6871eb4c29909a7494c380
Denis Kudia <dkudja@gmail.com>
2019-09-25 21:51:07 -0700

    Test TSL2561

--- pi-tests/test-i2c/test_i2c_tsl2561.cpp
+++ pi-tests/test-i2c/test_i2c_tsl2561.cpp
@@ -22,14 +22,28 @@ using namespace std;
 int main (int argc, char* argv[])
 {
     bool success = true;
-
+    uint32_t lux = 0;
+    int interval = 10;
     std::cout << "Started " << std::endl;
 
     std::cout << "Create provider" << std::endl;
     pi2c p_pi2c = std::make_shared<pirobot::i2c::I2C>();
 
     ptsl2561 p_ptsl2561 = std::make_shared<pirobot::item::Tsl2561>("TSL2561", p_pi2c);
+    p_ptsl2561->power_on(); //switch power On
 
+    while(interval > 0){
+        bool lux_res = p_ptsl2561->get_results(lux);
+        if(lux_res)
+           std::cout << lux << " lx" << std::endl;
+        else
+           std::cout << " Overflow" << std::endl;
+
+        sleep(1);
+        interval--;
+    }
+
+    p_ptsl2561->power_off(); //switch power Off
 
     std::cout << "Release TSL2561 objects " << std::endl;
     p_ptsl2561.reset();
