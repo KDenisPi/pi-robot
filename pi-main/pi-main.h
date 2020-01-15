de4d2b644e291bfa705bab2f2b59ac9ff32afd1e
KDenisPi <dkudja@gmail.com>
2020-01-14 21:02:40 -0800

    Main. Step two

--- pi-main/pi-main.h
+++ pi-main/pi-main.h
@@ -149,6 +149,180 @@ public:
 
 private:
 
+    const char* str_null = "/dev/null";
+    const char* str_weather_log = "/var/log/weather.log";
+    const char* str_weather_err = "/var/log/weather.err";
+
+    const int web_port = 8080;
+    /*
+    * Run application in daemon mode
+    */
+    void run_daemon(){
+
+        switch(_stmPid = fork()){
+        case -1:
+            std::cerr <<  "Could not create state machine application" << endl;
+            _exit(EXIT_FAILURE);
+
+        case 0: //child
+            //initialize daemon configuration
+            daemon_initialization();
+            //Initilize signal handlers            
+            initialize_signal_handlers();
+            //Initialize and run
+            initilize_and_run();
+
+            _exit(EXIT_SUCCESS);
+             break;
+
+        default: //parent
+            sleep(2);
+            //send command to start
+            kill(_stmPid, SIGUSR1);
+        }
+    }
+
+    /*
+    * Initialize objects and wait
+    */
+   void initilize_and_run(){
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + " Create child");
+        logger::set_level(_llevel);
+        /*
+        * Create PI Robot instance
+        */
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + "Create hardware support");
+        _pirobot = std::make_shared<pirobot::PiRobot>();
+        _pirobot->set_configuration(_robot_conf);
+
+        //Create State factory for State Machine
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + "Create State Factory support");
+        _factory = std::make_shared<weather::WeatherStFactory>(_firstState);
+        _factory->set_configuration(_robot_conf);
+
+        /*
+        * Create State machine
+        */
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + "Create state machine.");
+        _stm = std::make_shared<smachine::StateMachine>(_factory, _pirobot);
+
+        /*
+        * Web interface for settings and status
+        */
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + "Created Web interface");
+        _web = std::make_shared<weather::web::WebWeather>(web_port, _stm);
+        _web->http::web::WebSettings::start();
+
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + "Waiting for State Machine finishing");
+        _stm->wait();
+
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + "State machine finished");
+        _web->http::web::WebSettings::stop();
+
+        sleep(2);
+        _stm.reset();
+        _factory.reset();
+        _pirobot.reset();
+
+        logger::log(logger::LLOG::INFO, "main", std::string(__func__) + " Child finished");
+        logger::release();
+   }
+
+    /*
+    * Daemon initialization
+    */
+   void daemon_initialization(){
+        /*
+        * Become leader of new session
+        */
+        if (setsid() == -1)
+        _exit(EXIT_FAILURE);
+
+        /*
+        * Clear the process umask (Section 15.4.6), to ensure that, when the daemon creates
+        * files and directories, they have the requested permissions.
+        */
+        umask(0);
+
+        /*
+        * Change the process’s current working directory
+        */
+        if ( chdir("/") == -1 )
+          _exit(EXIT_FAILURE);
+
+        /*
+        * Close all opened file descriptors
+        */
+        int maxfd = sysconf(_SC_OPEN_MAX);
+        if (maxfd == -1) /* Limit is indeterminate... */
+          maxfd = BD_MAX_CLOSE;
+
+        /* so take a guess */
+        for (int fd = 0; fd < maxfd; fd++){
+          close(fd);
+        }
+
+        /*
+        * Reopen stdin, stdout, stderr
+        */
+        _fd_null = open(str_null, O_RDWR);
+        if (_fd_null != STDIN_FILENO) /* 'fd' should be 0 */
+          _exit(EXIT_FAILURE);
+
+        _fd_log = open(str_weather_log, O_RDWR|O_CREAT|O_APPEND, 0666);
+        if (_fd_log != STDOUT_FILENO) /* 'fd' should be 1 */
+          _exit(EXIT_FAILURE);
+
+        _fd_err = open(str_weather_err, O_RDWR|O_CREAT|O_APPEND, 0666);
+        if (_fd_err != STDERR_FILENO) /* 'fd' should be 2 */
+          _exit(EXIT_FAILURE);
+   }
+
+    /*
+    * Initilize handlers
+    */
+   void initialize_signal_handlers() {
+        /*
+        * Add handler for SIGALARM signal (used for State Machine Timers)
+        */
+        ADD_SIGNAL(SIGALRM)
+
+        /*
+        * Add handler  for SIGUSR2 signal - Used for State Machine finishing
+        */
+        if (signal(SIGUSR2, PiMain::sigHandlerStateMachine) == SIG_ERR ){
+          _exit(EXIT_FAILURE);
+        }
+
+        /*
+        * Add handler for SIGTERM signal - Used for State Machine finishing
+        */
+        if (signal(SIGTERM, PiMain::sigHandlerStateMachine) == SIG_ERR ){
+          _exit(EXIT_FAILURE);
+        }
+
+        /*
+        * Add handler for SIGINT signal - Used for State Machine finishing
+        */
+        if (signal(SIGINT, PiMain::sigHandlerStateMachine) == SIG_ERR ){
+          _exit(EXIT_FAILURE);
+        }
+
+        /*
+        * Add handler for SIGQUIT signal - Used for State Machine finishing
+        */
+        if (signal(SIGQUIT, sigHandlerStateMachine) == SIG_ERR ){
+          _exit(EXIT_FAILURE);
+        }
+
+        /*
+        * Add handler  for SIGUSR1 signal. This signal will be used for State Machine start.
+        */
+        if( signal(SIGUSR1, sigHandlerStateMachine) == SIG_ERR){
+          _exit(EXIT_FAILURE);
+        }
+   }
+
     /*
     *
     */
@@ -170,39 +344,45 @@ private:
         return filename;
     }
 
-/*
-* State Machine instance
-*/
-std::shared_ptr<smachine::StateMachine> _stm;
+    /*
+    * State Machine instance
+    */
+    std::shared_ptr<smachine::StateMachine> _stm;
 
-/*
-* State Machine process ID
-*/
-pid_t _stmPid;
+    /*
+    * State Machine process ID
+    */
+    pid_t _stmPid;
 
-/*
-* Daemon flag
-*/
-bool _daemon_mode = true;
+    /*
+    * Daemon flag
+    */
+    bool _daemon_mode = true;
 
-/*
-* Default debug level
-*/
-logger::LLOG _llevel = logger::LLOG::INFO;
+    /*
+    * Default debug level
+    */
+    logger::LLOG _llevel = logger::LLOG::INFO;
 
-/*
-* First state
-*/
-std::string _firstState = "StInitialization";
+    /*
+    * First state
+    */
+    std::string _firstState = "StInitialization";
 
-/*
-*
-*/
-std::string _robot_conf;    //main configuration file
-bool _use_mqtt = false;     //use MQTT flag
-std::string _mqtt_conf;     //MQTT configuration file
+    /*
+    *
+    */
+    std::string _robot_conf;    //main configuration file
+    bool _use_mqtt = false;     //use MQTT flag
+    std::string _mqtt_conf;     //MQTT configuration file
 
+    int _fd_log;
+    int _fd_null;
+    int _fd_err;
 
+    std::shared_ptr<pirobot::PiRobot> _pirobot;
+    std::shared_ptr<weather::WeatherStFactory> _factory;
+    std::shared_ptr<weather::web::WebWeather> _web
 };
 
 } //namespace pimain
