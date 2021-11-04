4f859ce6526df54b34f655e1651c1bdc36733bc9
Denis Kudia <dkudja@gmail.com>
2021-11-03 21:38:41 -0700

    Login with token

--- third-party.sh
+++ third-party.sh
@@ -87,3 +87,9 @@ apt-get install libcurl4-openssl-dev --assume-yes
 
 cd ../pi-robot
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+gh auth login --with-token < token.txt
