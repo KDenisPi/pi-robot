4641888b981e0f65517180286efd44cbbfe4a2d0
Denis Kudia <dkudja@gmail.com>
2024-05-20 21:08:38 -0700

    Small changes in BMP280 test

--- pi-tests/test-i2c/test_i2c_bmp280.cpp
+++ pi-tests/test-i2c/test_i2c_bmp280.cpp
@@ -15,20 +15,25 @@ int main (int argc, char* argv[])
     bool success = true;
 
     //std::cout << "Create provider" << std::endl;
+    logger::log(logger::LLOG::DEBUG, "BMP280", std::string(__func__) + " Started ");
     pi2c p_pi2c = std::make_shared<pirobot::i2c::I2C>();
     bmp280 p_bmp280 = std::make_shared<pirobot::item::Bmp280>("Bmp280", p_pi2c);
 
     const uint8_t status = p_bmp280->get_status();
+    std::cout << "Status: " << status << std::endl;
     float pressure, temp, altitude;
 
-    if(p_bmp280->get_results(pressure, temp, altitude)){
-        std::cout << "Results: Pressure: " << pressure << " Temp (C): " << temp << " Altitude: " << altitude << std::endl;
+    for(int i=0; i<5; i++){
+      if(p_bmp280->get_results(pressure, temp, altitude)){
+          std::cout << "Results: Pressure: " << pressure << " Temp (C): " << temp << " Altitude: " << altitude << std::endl;
+      }
+      else{
+          std::cout << "Could not get results" << std::endl;
+          success = false;
+          break;
+      }
+      sleep(2);
     }
-    else{
-        std::cout << "Could not get results" << std::endl;
-        success = false;
-    }
-
     std::cout << "Release Bmp280 objects " << std::endl;
     p_bmp280.reset();
     std::cout << "Release Provider objects " << std::endl;
