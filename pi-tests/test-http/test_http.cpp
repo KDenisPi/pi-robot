9b61e637708c4219c88a2d2f4dbddd132b53ff69
Denis Kudia <dkudja@gmail.com>
2024-11-18 17:55:19 -0800

    HTTP test. Initial step

---
+++ pi-tests/test-http/test_http.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory>
+
+#include "logger.h"
+
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+    bool success = true;
+
+
+    exit( (success ? EXIT_SUCCESS : EXIT_FAILURE));
+}
